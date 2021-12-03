2113a5a099dcae7660e0470780f63aaa9523ddb9
Steven Kristian <stevenkristian300900@gmail.com>
2021-12-03 17:03:38 +0700

    Created Sign Up Function

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,36 @@ router.get('/register', function(req, res, next){
   res.render('pages/register');
 })
 
+router.post('/username', function(req, res, next){
+  var { email } = req.body;
+  var { password } = req.body;
+  const { initializeApp } = require("firebase/app");
+  const { getAuth, createUserWithEmailAndPassword } = require("firebase/auth");
+
+  const firebaseApp = initializeApp({
+      apiKey: "AIzaSyDwAKkDF26AZa-x3hqZYaB4EFcxwGKOJOE",
+      authDomain: "sketsa-2bd5e.firebaseapp.com",
+      projectId: "sketsa-2bd5e",
+      storageBucket: "sketsa-2bd5e.appspot.com",
+      messagingSenderId: "270310736867",
+      appId: "1:270310736867:web:382525ebcd31f7b9b056d6",
+      measurementId: "G-PSZZT17JDE"
+  });
+
+  const auth = getAuth(firebaseApp);
+  createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          console.log(user);
+          // ...
+      })
+      .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ..
+      });
+  res.status(200).send("Succeed");
+})
+
 module.exports = router;
