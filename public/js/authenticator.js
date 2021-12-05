90e7805b53bbcf00a6be8f41cf754cc1cf11ae7d
Steven Kristian <stevenkristian300900@gmail.com>
2021-12-05 10:51:48 +0700

    Added Logout functionality

--- public/js/authenticator.js
+++ public/js/authenticator.js
@@ -1,90 +1,100 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged
-     } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js";
 
-const firebaseApp = initializeApp({
-    apiKey: "AIzaSyDwAKkDF26AZa-x3hqZYaB4EFcxwGKOJOE",
-    authDomain: "sketsa-2bd5e.firebaseapp.com",
-    projectId: "sketsa-2bd5e",
-    storageBucket: "sketsa-2bd5e.appspot.com",
-    messagingSenderId: "270310736867",
-    appId: "1:270310736867:web:382525ebcd31f7b9b056d6",
-    measurementId: "G-PSZZT17JDE"   
-});
+function getFirebaseAuthenticationApp(){
+    const firebaseApp = initializeApp({
+        apiKey: "AIzaSyDwAKkDF26AZa-x3hqZYaB4EFcxwGKOJOE",
+        authDomain: "sketsa-2bd5e.firebaseapp.com",
+        projectId: "sketsa-2bd5e",
+        storageBucket: "sketsa-2bd5e.appspot.com",
+        messagingSenderId: "270310736867",
+        appId: "1:270310736867:web:382525ebcd31f7b9b056d6",
+        measurementId: "G-PSZZT17JDE"   
+    });
+    
+    const auth = getAuth(firebaseApp);
+    return auth;
+}
 
-const auth = getAuth(firebaseApp);
 
 //user create an account with email and password when user clicks register button
-$("#register-button").click(function(){
+function register(){
+    const auth = getFirebaseAuthenticationApp();
+    var username = document.getElementById("username").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    console.log(email);
-    console.log(password);
     
+    //create the account
     createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            const user = userCredential.user;
+    .then((userCredential) => {    
+        //set the username of the user
+        const user = userCredential.user;
+        // window.location.replace('/dashboard');
+        updateProfile(user, {
+            displayName: username
+       })
+       .then(() => {
+            window.location.replace('/dashboard');
             console.log(user);
-        })  
-        .catch((error) => {
-            //show error message in the card html
-            var warning = $("#warning-message");
-            warning.css("color", "red");
-            var errorCode = error.code;
-            if(errorCode == "auth/email-already-in-use") {
-                warning.text("Email has already been used");
-            }
-            else if(errorCode == "auth/invalid-email") {
-                warning.text("Invalid Email");
-            }
-            else if(errorCode = "auth/weak-password") {
-                warning.text("Weak Password");
-            }
-            console.log(errorCode);
-        });
-});
+            console.log(`Successfully changed the user display name to ${username}`);
+       })
+       .catch(function(error){
+            console.log(error); 
+       })
+    })
+    .catch((error) => {
+        //show error message in the card html
+        var warning = $("#warning-message");
+        warning.css("color", "red");
+        var errorCode = error.code;
+        if(errorCode == "auth/email-already-in-use") {
+            warning.text("Email has already been used");
+        }
+        else if(errorCode == "auth/invalid-email") {
+            warning.text("Invalid Email");
+        }
+        else if(errorCode == "auth/weak-password") {
+            warning.text("Weak Password");
+        }
+    });
+};
 
 //user sign in by inputting their email and password when user clicks login button
-$("#login-button").click(function(){
+function login(){
+    const auth = getFirebaseAuthenticationApp();
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    console.log(email);
-    console.log(password);
     
     signInWithEmailAndPassword(auth, email, password)
     .then(function(userCredential){
         window.location.replace('/dashboard');
-        console.log("successful login");
     })
     .catch((error) => {
         //show error message in the card html
         var warning = $("#warning-message");
         warning.css("color", "red");
         const errorCode = error.code;
-        console.log(errorCode);
         if(errorCode == "auth/user-not-found" || errorCode == "auth/invalid-email"){
             warning.text("Email not found");
         }
         else if(errorCode == "auth/wrong-password"){
             warning.text("Wrong password");
         } 
+        console.log(errorCode);
     });
-});
+};
 
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        var email = user.email;
-        var greetingText = $("#greeting-text");
-        console.log(user);
-        console.log(`User email is ${email}`);
-        greetingText.text(`Good morning ${email}`);
-        // ...
-    } else {
-        // User is signed out
-        console.log("No user found");
-        // ...
-    }
+function logout(){
+    const auth = getFirebaseAuthenticationApp();
+    signOut(auth)
+    .then(() => {
+        window.location.replace('/');
+    })
+    .catch((error) => {
+        console.log("Not logged in yet");
     });
+}
 
-
-
+$("#logout-button").click(logout);
+$("#login-button").click(login);
+$("#register-button").click(register);
---
+++ public/js/dashboard.js
@@ -0,0 +1,39 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js";
+
+//check if a user has been signed in
+function initDashboard(){
+    const firebaseApp = initializeApp({
+        apiKey: "AIzaSyDwAKkDF26AZa-x3hqZYaB4EFcxwGKOJOE",
+        authDomain: "sketsa-2bd5e.firebaseapp.com",
+        projectId: "sketsa-2bd5e",
+        storageBucket: "sketsa-2bd5e.appspot.com",
+        messagingSenderId: "270310736867",
+        appId: "1:270310736867:web:382525ebcd31f7b9b056d6",
+        measurementId: "G-PSZZT17JDE"   
+    });
+    
+    const auth = getAuth(firebaseApp);
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            if(window.location.href != "http://localhost:8000/dashboard"){
+                window.location.replace("/dashboard")
+            }
+            var username = user.displayName;
+            var greetingText = $("#greeting-text");
+            console.log(user);
+            greetingText.text(`Good Morning, ${username}!`);
+            // ...
+        } else {
+            // User is signed out
+            console.log("No user found");
+            // ...
+        }
+    });
+    
+}
+
+
+window.onload = function() {
+    initDashboard();
+};
